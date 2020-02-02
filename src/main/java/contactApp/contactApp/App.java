8ce4e1f1e4ed4c4fc7ad8049538eb460ed63d6e1
ndemanou <ndemanousteve@gmail.com>
2020-02-03 00:37:16 +0100

    iniatilze maven project with some dependancies

---
+++ src/main/java/contactApp/contactApp/App.java
@@ -0,0 +1,13 @@
+package contactApp.contactApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
